baebfe91f3cf78e5ae6c1ef8607c6401d661e1d3
NopalAul <mnaufal0605@gmail.com>
2024-02-07 11:25:23 +0700

    feat: add input from CLI

--- readFile.py
+++ readFile.py
@@ -1,53 +1,113 @@
-# Read from txt file
+# Input reader file
 import os
 from pathlib import Path
+import random
 
-# Open file
-path = os.path.dirname(Path(__file__).absolute())
-print(f'path: {path}')
+# ------------ Input from txt file
+def input_from_file():
+    # Open file
+    path = os.path.dirname(Path(__file__).absolute())
+    print(f'path: {path}')
 
-source_file = "soal.txt"
-file_name = os.path.join(path, 'test' ,source_file)
+    source_file = "soal.txt"
+    file_name = os.path.join(path, 'test' ,source_file)
 
-# Read file
-with open(file_name, "r") as file:
-    buffer_size = int(file.readline().strip())
-    matrix_size = file.readline().split()
-    matrix_width = int(matrix_size[0])
-    matrix_height = int(matrix_size[1])
+    # Read file
+    with open(file_name, "r") as file:
+        buffer_size = int(file.readline().strip())
+        matrix_size = file.readline().split()
+        matrix_width = int(matrix_size[0])
+        matrix_height = int(matrix_size[1])
 
-    matrix = []
+        matrix = []
 
-    for i in range(matrix_height):
-        matrix.append(file.readline().strip().split())
+        for i in range(matrix_height):
+            matrix.append(file.readline().strip().split())
 
-    n_sequences = int(file.readline().strip())
+        n_sequences = int(file.readline().strip())
 
-    sequences_and_reward = []
-    sequence = []
-    # Data: [[[sequence1], reward1], [[sequence2], reward2], ...]
-    for i in range(n_sequences):
-        sequence = [file.readline().strip().split()]
-        sequence.append(int(file.readline().strip()))
-        sequences_and_reward.append(sequence)
-        # print(f'len matrix: {len(matrix)}')
+        # Data: [[[sequence1], reward1], [[sequence2], reward2], ...]
+        sequences_and_reward = []
+        sequence = []
+        for i in range(n_sequences):
+            sequence = [file.readline().strip().split()]
+            sequence.append(int(file.readline().strip()))
+            sequences_and_reward.append(sequence)
+            # print(f'len matrix: {len(matrix)}')
+        
+        # # Data: 2 array: 1 array for sequence, 1 array for reward
+        # sequences_and_reward = []
+        # sequence_list = []
+        # reward_list = []
+        # for i in range(n_sequences):
+        #     sequence = file.readline().strip().split()
+        #     reward_list.append(int(file.readline().strip()))
+        #     sequences_and_reward.append(sequence)
+        #     # print(f'len matrix: {len(matrix)}')
 
-# Test, del
-print(f'sequences: {sequence}')
-print(f'sequences_and_reward: {sequences_and_reward}')
-print(f'seq reward 1: {sequences_and_reward[0]}')
-print(f'seq 1 tokens: {sequences_and_reward[0][0]}')
-print(f'seq 1 tokens 1: {sequences_and_reward[0][0][0]}')
-print(f'seq 1 reward: {sequences_and_reward[0][1]}')
-print(f'n_sequences: {n_sequences}')
-print(f'matrix: {matrix}') 
-print(f'first row: {matrix[0]}')    
-print(f'first row first collumn: {matrix[0][0]}')   
-print(f'buffer_size: {buffer_size}')
-print(f'matrix_size: {matrix_size}')
-print(f'matrix_width: {matrix_width}')
-print(f'matrix_height: {matrix_height}')
+    # Test, del
+    print(f'sequences: {sequence}')
+    print(f'sequences_and_reward: {sequences_and_reward}')
+    # print(f'reward_list: {reward_list}')
+    print(f'seq reward 1: {sequences_and_reward[0]}')
+    print(f'seq 1 tokens: {sequences_and_reward[0][0]}')
+    print(f'seq 1 tokens 1: {sequences_and_reward[0][0][0]}')
+    print(f'seq 1 reward: {sequences_and_reward[0][1]}')
+    print(f'n_sequences: {n_sequences}')
+    print(f'matrix: {matrix}') 
+    print("Matrix:")
+    print_matrix(matrix)
+    print(f'first row: {matrix[0]}')    
+    print(f'first row first collumn: {matrix[0][0]}')   
+    print(f'buffer_size: {buffer_size}')
+    print(f'matrix_size: {matrix_size}')
+    print(f'matrix_width: {matrix_width}')
+    print(f'matrix_height: {matrix_height}')
 
 
+# --------------- Input from CLI
+def input_from_cli():
+    n_token = int(input("Jumlah token: "))
+    token = str(input("Token: "))
+    buffer_size = int(input("Ukuran buffer: "))
+    matrix_size = str(input("Ukuran matrix (m n): "))
+    n_sequences = int(input("Jumlah sequence: "))
+    max_sequence_size = int(input("Ukuran maksimal sequence: "))
+
+    token_arr = token.split()
+    matrix_width = int(matrix_size.split()[0])
+    matrix_height = int(matrix_size.split()[1])
+
+    # Matrix generator
+    matrix = [['' for i in range(matrix_height)] for j in range(matrix_width)]
+    print('\nMatrix:')
+    for i in range(matrix_width):
+        for j in range(matrix_height):
+            random_token = random.choice(token_arr) 
+            matrix[i][j] = random_token
 
 
+    # test 
+    print()
+    print(f'matrix: {matrix}')
+    print("Matrix:")
+    print_matrix(matrix)
+    print(f'token_arr: {token_arr}')
+    print(f'matrix_width: {matrix_width}')
+    print(f'matrix_height: {matrix_height}')
+    print(f'n_token: {n_token}')
+    print(f'token: {token}')
+    print(f'buffer_size: {buffer_size}')
+    print(f'matrix_size: {matrix_size}')
+    print (f'n_sequences: {n_sequences}')
+    print(f'max_sequence_size: {max_sequence_size}')
+
+
+# print matrix
+def print_matrix(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            print(f'{matrix[i][j]}', end=' ')
+        print()
+
+input_from_cli()
